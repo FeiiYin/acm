4f611d45bc4aef6fea3e931ac2dccc7db72782c0
Samaritan <1594320575@qq.com>
2018-09-15 18:50:13 +0800

    Create 焦作网络赛2018 I.Save the Room | 签到.cpp

---
+++ jisuanke/焦作网络赛2018 I.Save the Room | 签到.cpp
@@ -0,0 +1,57 @@
+/*************************************************************************
+    > File Name: i.cpp
+    > Author: BlockChanZJ
+    > Mail: 386636627@qq.com 
+    > Created Time: 2018年09月15日 12:04:42
+ ************************************************************************/
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <ctime>
+#include <queue>
+#include <cassert>
+#include <iomanip>
+using namespace std;
+
+#define rep(i,a,b) for(int i = (a); i <= (b); ++i)
+#define per(i,a,b) for(int i = (a); i >= (b); --i)
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define fi first 
+#define se second
+#define SZ(x) ((int)(x).size())
+#define sqr(x) (x) * (x)
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> PII;
+typedef vector<int> VI;
+typedef pair<PII,int> PIII;
+const int N = 100005;
+const int P = 1000000007;
+const ll mod = P;
+const int INF = 0x3f3f3f3f;
+const ll LLINF = 0x3f3f3f3f3f3f3f3f;
+const ld eps = 1e-8;
+ll qmod(ll a,ll n,ll mod){ll ans=1;while(n){assert(ans>0);if(n&1ll)ans=ans*a%mod;n>>=1;a=a*a%mod;}return ans%mod;}
+
+//head
+
+//your code goes there
+ 
+int main() {
+	int a, b, c;
+	while(cin >> a >> b >> c) {
+		if(a*b*c%2==1) cout<<"No\n";
+		else cout<<"Yes\n";
+	}
+	return 0;
+}
