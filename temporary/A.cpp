517f40551da1bff8594f0ecb8f177af6df2d0883
Samaritan <1594320575@qq.com>
2019-11-11 13:21:19 +0800

    Create A.cpp

---
+++ temporary/A.cpp
@@ -0,0 +1,36 @@
+/**
+ * @Samaritan
+ */
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+
+
+//const int N = 5e5 + 5;
+//int cost[N];
+//int dp[N];
+//int que[N];
+void show(int arr[], int n) {
+    for (int i = 0; i < n; ++i) cout << arr[i] << " "; cout << "\n";
+}
+
+//bool check(int n, int k) {
+//
+//}
+int main() {
+    int kase;
+    scanf("%d", &kase);
+    while (kase--) {
+        int n;
+        scanf("%d", &n);
+//        int t = n;
+//        int sum = 0;
+//        while (t) {
+//            sum += t % 10;
+//            t /= 10;
+//        }
+        for (int i = 0; i < n; ++i) printf("%d", n);
+        printf("\n");
+    }
+    return 0;
+}
